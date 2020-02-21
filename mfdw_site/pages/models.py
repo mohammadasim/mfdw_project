cbc06bf457cc4b834eeacb0457ac4d8de7d8f6e7
mohammad.ayub <asimayub81@gmail.com>
2020-02-21 09:10:45 +0000

    added favicon and working view

--- mfdw_site/pages/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Page(models.Model):
-    title = models.CharField(max_length=60)
-    permalink = models.CharField(max_length=12,
-                                 unique=True)
-    update_date = models.DateTimeField('Last Updated')
-    bodytext = models.TextField('Page Content', blank=True)
--- mfdw_site/pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("<h1> The Meandco HomePage</h1>")
