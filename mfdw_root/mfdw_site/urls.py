e49d29c0b5f590d78e39060f9bc8d8dd9b96da77
mohammad.ayub <asimayub81@gmail.com>
2020-02-28 09:08:09 +0000

    added authentication system

--- mfdw_root/mfdw_site/urls.py
+++ mfdw_root/mfdw_site/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from quotes.views import Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/success/', TemplateView.
+         as_view(template_name="registration/success.html"), name='register-success'),
+    path('register', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
--- mfdw_root/quotes/views.py
+++ mfdw_root/quotes/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from pages.models import Page
 
@@ -14,6 +18,8 @@ def quote_req(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/quote/?submitted=True')
+        else:
+            return render(request, "quotes/quote.html", {'form': form})
     else:
         form = QuoteForm()
         if 'submitted' in request.GET:
@@ -31,3 +37,23 @@ class QuoteList(ListView):
         context = super(QuoteList, self).get_context_data()
         context['page_list'] = Page.objects.all()
         return context
+
+
+class QuoteView(DetailView):
+    model = Quote
+    context_object_name = 'quote'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_invalid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
